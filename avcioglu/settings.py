e83fe2623f4731d02ffa90c2e859c91999cd0631
Soner Cem Kebiz <soncem_95@hotmail.com>
2020-12-30 21:57:15 +0300

    Order Email System Sendgrid Integration v4

--- avcioglu/settings.py
+++ avcioglu/settings.py
@@ -157,9 +157,8 @@ EMAIL_HOST = env.str('EMAIL_HOST')
 EMAIL_PORT = env.int('EMAIL_PORT')
 EMAIL_SEND_USER = env.str('EMAIL_SEND_USER')
 EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
 
-# env.str('EMAIL_HOST_PASSWORD')
 
 try:
     from avcioglu.local_settings import *
